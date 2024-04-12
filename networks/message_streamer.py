167a93478e05643134a8cef55aa6fcd67c46d032
AsadIqbalAI <152972457+AsadIqbalAI@users.noreply.github.com>
2024-04-12 15:34:46 +0500

    Update message_streamer.py

--- networks/message_streamer.py
+++ networks/message_streamer.py
@@ -12,6 +12,7 @@ from constants.models import (
     TOKEN_RESERVED,
 )
 from messagers.message_outputer import OpenaiStreamOutputer
+from utils.logger import logger
 from utils.enver import enver
 
 
@@ -26,6 +27,8 @@ class MessageStreamer:
         self.message_outputer = OpenaiStreamOutputer()
 
         if self.model == "gemma-7b":
+            # this is not wrong, as repo `google/gemma-7b-it` is gated and must authenticate to access it
+            # so I use mistral-7b as a fallback
             self.tokenizer = AutoTokenizer.from_pretrained(MODEL_MAP["mistral-7b"])
         else:
             self.tokenizer = AutoTokenizer.from_pretrained(self.model_fullname)
@@ -37,12 +40,13 @@ class MessageStreamer:
         try:
             content = data["token"]["text"]
         except:
-            pass
+            logger.err(data)
         return content
 
     def count_tokens(self, text):
         tokens = self.tokenizer.encode(text)
         token_count = len(tokens)
+        logger.note(f"Prompt Token Count: {token_count}")
         return token_count
 
     def chat_response(
@@ -54,6 +58,8 @@ class MessageStreamer:
         api_key: str = None,
         use_cache: bool = False,
     ):
+        # https://huggingface.co/docs/api-inference/detailed_parameters?code=curl
+        # curl --proxy http://<server>:<port> https://api-inference.huggingface.co/models/<org>/<model_name> -X POST -d '{"inputs":"who are you?","parameters":{"max_new_token":64}}' -H 'Content-Type: application/json' -H 'Authorization: Bearer <HF_TOKEN>'
         self.request_url = (
             f"https://api-inference.huggingface.co/models/{self.model_fullname}"
         )
@@ -62,10 +68,14 @@ class MessageStreamer:
         }
 
         if api_key:
+            logger.note(
+                f"Using API Key: {api_key[:3]}{(len(api_key)-7)*'*'}{api_key[-4:]}"
+            )
             self.request_headers["Authorization"] = f"Bearer {api_key}"
 
         if temperature is None or temperature < 0:
             temperature = 0.0
+        # temperature must  0 < and < 1 for HF LLM models
         temperature = max(temperature, 0.01)
         temperature = min(temperature, 0.99)
         top_p = max(top_p, 0.01)
@@ -82,6 +92,13 @@ class MessageStreamer:
         else:
             max_new_tokens = min(max_new_tokens, token_limit)
 
+        # References:
+        #   huggingface_hub/inference/_client.py:
+        #     class InferenceClient > def text_generation()
+        #   huggingface_hub/inference/_text_generation.py:
+        #     class TextGenerationRequest > param `stream`
+        # https://huggingface.co/docs/text-generation-inference/conceptual/streaming#streaming-with-curl
+        # https://huggingface.co/docs/api-inference/detailed_parameters#text-generation-task
         self.request_body = {
             "inputs": prompt,
             "parameters": {
@@ -98,7 +115,12 @@ class MessageStreamer:
 
         if self.model in STOP_SEQUENCES_MAP.keys():
             self.stop_sequences = STOP_SEQUENCES_MAP[self.model]
+        #     self.request_body["parameters"]["stop_sequences"] = [
+        #         self.STOP_SEQUENCES[self.model]
+        #     ]
 
+        logger.back(self.request_url)
+        enver.set_envs(proxies=True)
         stream_response = requests.post(
             self.request_url,
             headers=self.request_headers,
@@ -108,9 +130,9 @@ class MessageStreamer:
         )
         status_code = stream_response.status_code
         if status_code == 200:
-            pass
+            logger.success(status_code)
         else:
-            pass
+            logger.err(status_code)
 
         return stream_response
 
@@ -126,18 +148,22 @@ class MessageStreamer:
                 },
             }
         ]
+        logger.back(final_output)
 
         final_content = ""
-        token_count = 0
+        token_count = 0  # New variable to count tokens
         for line in stream_response.iter_lines():
             if not line:
                 continue
             content = self.parse_line(line)
 
             if content.strip() == self.stop_sequences:
+                logger.success("\n[Finished]")
                 break
             else:
+                logger.back(content, end="")
                 final_content += content
+                # Increment token count
                 token_count += self.count_tokens(content)
 
         if self.model in STOP_SEQUENCES_MAP.keys():
@@ -146,7 +172,9 @@ class MessageStreamer:
         final_content = final_content.strip()
         final_output["choices"][0]["message"]["content"] = final_content
 
+        # Include token count in the output
         final_output["tokens_used"] = token_count
+        logger.note(f"Total Tokens Used: {token_count}")
 
         return final_output
         
@@ -163,11 +191,13 @@ class MessageStreamer:
 
             if content.strip() == self.stop_sequences:
                 content_type = "Finished"
+                logger.success("\n[Finished]")
                 is_finished = True
             else:
                 content_type = "Completions"
                 if line_count == 1:
                     content = content.lstrip()
+                logger.back(content, end="")
 
             output = self.message_outputer.output(
                 content=content, content_type=content_type
