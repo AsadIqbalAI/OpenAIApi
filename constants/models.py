e90647802c318ffd153942bfcfa30a90ef6f2bd5
AsadIqbalAI <152972457+AsadIqbalAI@users.noreply.github.com>
2024-04-18 18:11:32 +0500

    Update models.py

--- constants/models.py
+++ constants/models.py
@@ -6,12 +6,15 @@ MODEL_MAP = {
     "gemma-7b": "google/gemma-1.1-7b-it",
     "command-r-plus": "CohereForAI/c4ai-command-r-plus",
     "default": "mistralai/Mixtral-8x7B-Instruct-v0.1",
+    "zephyr-orpo-GPT4":"HuggingFaceH4/zephyr-orpo-141b-A35b-v0.1",
+
 }
 
 AVAILABLE_MODELS = list(MODEL_MAP.keys())
 
 STOP_SEQUENCES_MAP = {
     "mixtral-8x7b": "</s>",
+    "zephyr-orpo-GPT4":"</s>,
     "nous-mixtral-8x7b": "<|im_end|>",
     "mistral-7b": "</s>",
     "openchat-3.5": "<|end_of_turn|>",
@@ -20,6 +23,7 @@ STOP_SEQUENCES_MAP = {
 }
 
 TOKEN_LIMIT_MAP = {
+    "zephyr-orpo-GPT4":32768,
     "mixtral-8x7b": 32768,
     "nous-mixtral-8x7b": 32768,
     "mistral-7b": 32768,
@@ -38,49 +42,47 @@ AVAILABLE_MODELS_DICTS = [
         "id": "mixtral-8x7b",
         "description": "[mistralai/Mixtral-8x7B-Instruct-v0.1]: https://huggingface.co/mistralai/Mixtral-8x7B-Instruct-v0.1",
         "object": "model",
-        "created": 1700000000,
         "owned_by": "mistralai",
     },
     {
         "id": "nous-mixtral-8x7b",
         "description": "[NousResearch/Nous-Hermes-2-Mixtral-8x7B-DPO]: https://huggingface.co/NousResearch/Nous-Hermes-2-Mixtral-8x7B-DPO",
         "object": "model",
-        "created": 1700000000,
         "owned_by": "NousResearch",
     },
     {
         "id": "mistral-7b",
         "description": "[mistralai/Mistral-7B-Instruct-v0.2]: https://huggingface.co/mistralai/Mistral-7B-Instruct-v0.2",
         "object": "model",
-        "created": 1700000000,
         "owned_by": "mistralai",
     },
     {
         "id": "openchat-3.5",
         "description": "[openchat/openchat-3.5-0106]: https://huggingface.co/openchat/openchat-3.5-0106",
         "object": "model",
-        "created": 1700000000,
         "owned_by": "openchat",
     },
     {
         "id": "gemma-7b",
         "description": "[gemma-1.1-7b-it]: https://huggingface.co/google/gemma-1.1-7b-it",
         "object": "model",
-        "created": 1700000000,
         "owned_by": "Google",
     },
     {
         "id": "command-r-plus",
         "description": "[CohereForAI/c4ai-command-r-plus]: https://huggingface.co/CohereForAI/c4ai-command-r-plus",
-        "object": "model",
-        "created": 1700000000,
+        "object": "model",,
         "owned_by": "CohereForAI",
     },
+    {
+        "id":"zephyr-orpo-GPT4",
+        "desciption":"[meta-llama/Llama-2-70b-chat-hf]: https://huggingface.co/meta-llama/Llama-2-70b-chat-hf",
+        "object":"model",
+        "owned_by":"Meta",
     {
         "id": "gpt-3.5-turbo",
         "description": "[openai/gpt-3.5-turbo]: https://platform.openai.com/docs/models/gpt-3-5-turbo",
         "object": "model",
-        "created": 1700000000,
         "owned_by": "OpenAI",
     },
 ]
